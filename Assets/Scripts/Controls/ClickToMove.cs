3eba16687ad6020dc4d7b0d25985e622e11e45ea
Andrey Vysher <andrey@Andreys-MacBook-Pro.local>
2015-01-14 21:24:17 +0200

    Add new training sceen, add MoveToClick script

---
+++ Assets/Scripts/Controls/ClickToMove.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickToMove : MonoBehaviour {
+
+	public CharacterController controller;
+	public float speed = 10f;
+	private Vector3 clickPosition;
+	// Use this for initialization
+	void Start () {
+		clickPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(1)) {
+			// Locate a palyer click:
+			LocateClickPosition ();
+		}
+
+		// Move player to the clickPosition:
+		MoveToPosition ();
+	}
+
+	void LocateClickPosition () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit, 1000)) {
+			clickPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+			Debug.Log(clickPosition);
+		}
+	}
+
+	void MoveToPosition () {
+		if (Vector3.Distance (transform.position, clickPosition) > 1) {
+			Quaternion newRotation = Quaternion.LookRotation (clickPosition - transform.position);
+
+			newRotation.x = 0f;
+			newRotation.z = 0f;
+
+			transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 10);
+			controller.SimpleMove (transform.forward * speed);
+		}
+	}
+}
