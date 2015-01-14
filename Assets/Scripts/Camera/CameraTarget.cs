61f1a569f5e515ce3698d44185697d2ebe073a41
Alexander Vasileyko <alexnodejs@yahoo.com>
2015-01-14 23:57:37 +0200

    Normalize scale and add camera rotation + zoom

--- Assets/Scripts/Camera/CameraTarget.cs
+++ Assets/Scripts/Camera/CameraTarget.cs
@@ -1,57 +1,111 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraTarget : MonoBehaviour {
-    
-	// Camera movement speed
-    public float _MoveSpeed;
-
-	// Scene limits
-	public int _MinX, _MinZ, _MaxX, _MaxZ;
+public class CameraTarget : MonoBehaviour
+{
 
     // Border on screen edges where we can move the camera
-    private int _Border = 5;
+    private static int ACTIVE_SCREEN_BORDER_WIDTH = 5;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Camera movement speed
+    public float movementSpeed;
 
-	// Update is called once per frame
-	void Update () {
-		moveCamera (Input.mousePosition.x, Input.mousePosition.y);
+    // Scene limits
+    public int minX, minZ, maxX, maxZ;
 
-		// TODO: ZOOM
-		// TODO: ROTATION
-	}
-	
-	// Move camera nothing more
-	private void moveCamera (float mouseX, float mouseY) {
-		var previousPosition = transform.position;
+    // Camera zooming limits
+    public int zoomMin, zoomMax;
 
-		if (mouseX < _Border) {
-			transform.Translate(Vector3.right * -_MoveSpeed * Time.deltaTime);
-		}
-		if (mouseX >= Screen.width - _Border) {
-			transform.Translate(Vector3.right * _MoveSpeed * Time.deltaTime);
-		}
-		if (mouseY < _Border) {
-			transform.Translate(Vector3.forward * -_MoveSpeed * Time.deltaTime);
-		}
-		if (mouseY >= Screen.height - _Border) {
-			transform.Translate(Vector3.forward * _MoveSpeed * Time.deltaTime);
-		}
+    // Camera zooming speed
+    public float zoomSpeed;
 
-		preventInfiniteMovingOfCamera (previousPosition, transform.position);
-	}
+    // Camera rotation speed
+    public float rotateSpeed;
 
-	// Stop camera when it reaches scene limits and return it to the previous position 
-	private void preventInfiniteMovingOfCamera (Vector3 previousPosition, Vector3 currentPosition) {
-		if (currentPosition.z < _MinZ || currentPosition.z > _MaxZ) {
-			transform.position = previousPosition;
-		}
-		if (currentPosition.x < _MinX || currentPosition.x > _MaxX) {
-			transform.position = previousPosition;
-		}
-	}
+    // Mouse x position for camera rotation
+    private float mouseXPositionForRotation;
+
+    // Use this for initialization
+    void Start()
+    {
+        // initialize something
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCamera(Input.mousePosition.x, Input.mousePosition.y);
+        zoomCamera();
+        rotateCamera();
+    }
+
+    // Update is called once per frame after Update() was called
+    void LateUpdate()
+    {
+        mouseXPositionForRotation = Input.mousePosition.x;
+    }
+
+    // Move camera by mouse
+    private void moveCamera(float mouseX, float mouseY)
+    {
+        var previousPosition = transform.position;
+
+        if (mouseX < ACTIVE_SCREEN_BORDER_WIDTH)
+        {
+            transform.Translate(Vector3.right * -movementSpeed * Time.deltaTime);
+        }
+        if (mouseX >= Screen.width - ACTIVE_SCREEN_BORDER_WIDTH)
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+        if (mouseY < ACTIVE_SCREEN_BORDER_WIDTH)
+        {
+            transform.Translate(Vector3.forward * -movementSpeed * Time.deltaTime);
+        }
+        if (mouseY >= Screen.height - ACTIVE_SCREEN_BORDER_WIDTH)
+        {
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+
+        preventInfiniteMovingOfCamera(previousPosition, transform.position);
+    }
+
+    // Stop camera when it reaches scene limits and return it to the previous position 
+    private void preventInfiniteMovingOfCamera(Vector3 previousPosition, Vector3 currentPosition)
+    {
+        if (currentPosition.z < minZ || currentPosition.z > maxZ)
+        {
+            transform.position = previousPosition;
+        }
+        if (currentPosition.x < minX || currentPosition.x > maxX)
+        {
+            transform.position = previousPosition;
+        }
+    }
+
+    // Zoom camera by scrolling the wheel
+    private void zoomCamera()
+    {
+        if (transform.position.y > zoomMin && Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            transform.Translate(0, -zoomSpeed, zoomSpeed);
+        }
+        if (transform.position.y < zoomMax && Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            transform.Translate(0, zoomSpeed, -zoomSpeed);
+        }
+    }
+
+    // Rotate camera by shift + right mouse button
+    private void rotateCamera()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetMouseButton(1))
+        {
+            if (Input.mousePosition.x != mouseXPositionForRotation)
+            {
+                var rotation = (Input.mousePosition.x - mouseXPositionForRotation) * rotateSpeed * Time.deltaTime;
+                transform.Rotate(0, rotation, 0);
+            }
+        }
+    }
 }
--- Assets/Scripts/Controls/ClickToMove.cs
+++ Assets/Scripts/Controls/ClickToMove.cs
@@ -1,47 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ClickToMove : MonoBehaviour {
+public class ClickToMove : MonoBehaviour
+{
+    public CharacterController controller;
+    
+    public float speed = 10f;
+    
+    private Vector3 clickPosition;
+    
+    // Use this for initialization
+    void Start()
+    {
+        clickPosition = transform.position;
+    }
 
-	public CharacterController controller;
-	public float speed = 10f;
-	private Vector3 clickPosition;
-	// Use this for initialization
-	void Start () {
-		clickPosition = transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-		if (Input.GetMouseButton(1)) {
-			// Locate a palyer click:
-			LocateClickPosition ();
-		}
+        if (Input.GetMouseButton(1))
+        {
+            // Locate a palyer click:
+            LocateClickPosition();
+        }
 
-		// Move player to the clickPosition:
-		MoveToPosition ();
-	}
+        // Move player to the clickPosition:
+        MoveToPosition();
+    }
 
-	void LocateClickPosition () {
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+    void LocateClickPosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		if (Physics.Raycast(ray, out hit, 1000)) {
-			clickPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
-			Debug.Log(clickPosition);
-		}
-	}
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            clickPosition = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+            Debug.Log(clickPosition);
+        }
+    }
 
-	void MoveToPosition () {
-		if (Vector3.Distance (transform.position, clickPosition) > 1) {
-			Quaternion newRotation = Quaternion.LookRotation (clickPosition - transform.position);
+    void MoveToPosition()
+    {
+        if (Vector3.Distance(transform.position, clickPosition) > 1)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(clickPosition - transform.position);
 
-			newRotation.x = 0f;
-			newRotation.z = 0f;
+            newRotation.x = 0f;
+            newRotation.z = 0f;
 
-			transform.rotation = Quaternion.Slerp (transform.rotation, newRotation, Time.deltaTime * 10);
-			controller.SimpleMove (transform.forward * speed);
-		}
-	}
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 10);
+            controller.SimpleMove(transform.forward * speed);
+        }
+    }
 }
