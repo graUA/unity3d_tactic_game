f4a496667bc3ddde9303f2c3a68478239b336c72
Andrey Vysher <andrey@Andreys-MacBook-Pro.local>
2015-01-16 21:07:42 +0200

    Add 'Global' namespace

---
+++ Assets/Scripts/Global/Global.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Global
+{ 
+	public static class Const
+	{
+		public static const GAME_NAME = "Space Hunta";
+		public static const GAME_VERSION = "0.1";
+	}
+}
