0fcce07ac7e0c6ec0d9172c00756e3cb25ec5cf7
Andrey Vysher <andrey@Andreys-MacBook-Pro.local>
2015-01-16 21:09:39 +0200

    Fix Global namespace

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -5,7 +5,7 @@ namespace Global
 { 
 	public static class Const
 	{
-		public static const GAME_NAME = "Space Hunta";
-		public static const GAME_VERSION = "0.1";
+		public static const string GAME_NAME = "Space Hunta";
+		public static const string GAME_VERSION = "0.1";
 	}
 }
