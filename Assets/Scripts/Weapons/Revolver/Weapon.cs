cdf2423c0e769cd1cde1624b3314826f5d4776cf
Andrey Vysher <andrey@Andreys-MacBook-Pro.local>
2015-02-04 00:09:05 +0200

    Start work on enemy AI and Waepon behavior test sceene

---
+++ Assets/Scripts/Weapons/Revolver/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour 
+{
+	public float shotDamage = 10f;
+	public float shotSpeed = 0.5f;
+	public float ammoCount = 10f;
+	public GameObject shotPointObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Weapons/WeaponBehavior.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Global;
+
+public interface IWeaponBehavior
+{
+	void Shoot();
+	bool CanIShoot();
+}
+
+public class WeaponBehavior
+{
+	private IWeaponBehavior _weaponBehavior;
+
+	public WeaponBehavior(IWeaponBehavior weaponBehavior)
+	{
+		_weaponBehavior = weaponBehavior;
+	}
+
+	public void SetBehavior(IWeaponBehavior weaponBehavior)
+	{
+		_weaponBehavior = weaponBehavior;
+	}
+
+	public void Shoot()
+	{
+		_weaponBehavior.Shoot();
+	}
+
+	public bool CanIShoot()
+	{
+		return _weaponBehavior.CanIShoot();
+	}
+}
+
+public class RevolverBehavior : IWeaponBehavior
+{
+	public void Shoot()
+	{
+
+	}
+
+	public bool CanIShoot()
+	{
+		return true;
+	}
+}
