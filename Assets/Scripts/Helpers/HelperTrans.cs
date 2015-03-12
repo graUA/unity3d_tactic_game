9301e719e739fa00398c8269b58fa858d94c60bc
unknown <andrey.v.zp@gmail.com>
2015-03-12 20:18:19 +0200

    Fix spelling bug

--- Assets/Scripts/Helpers/HelperTrans.cs
+++ Assets/Scripts/Helpers/HelperTrans.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 using Global;
 
 public static class HelperTrans
 {
-	public static float angeBetwineTwoTransforms(Transform transformA, Transform transformB)
+	public static float angleBetwineTwoTransforms(Transform transformA, Transform transformB)
 	{
 		Vector3 direction = transformA.position -transformB.position;
 		float angle = Vector3.Angle(direction, transformB.forward);
